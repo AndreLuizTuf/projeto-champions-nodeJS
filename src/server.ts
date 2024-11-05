66c2f74046b76455e22ef6aa86c0350b5730e96d
André Luiz <andre.luiz.a.f@hotmail.com>
2024-11-05 10:10:07 -0300

    Configurações do Express

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
-import express, { Request, Response} from 'express';
+import express, { Request, Response } from "express";
 
-const app = express()
+const app = express();
+const port = process.env.PORT;
 
 app.get("/", (req: Request, res: Response) => {
-    res.send("Hello World!");
-})
+  res.status(200).json({ player: "beckham" });
+});
 
-app.listen(3000)
+app.listen(port, () => {
+  console.log(`🔥 Server running at port http://localhost:${port}`);
+});
