778bc84d3f853b91ae6683b574fe814a3becce5c
André Luiz <andre.luiz.a.f@hotmail.com>
2024-11-05 15:36:01 -0300

    Get player by id

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,14 +1,29 @@
 import * as PlayerRepostory from "../repositories/players-repository";
-import { noContent, ok } from "../utils/http-helpers";
+import * as HttpResponse from "../utils/http-helpers";
 
 export const getPlayerService = async () => {
   const data = await PlayerRepostory.findAllPlayers();
   let response = null;
 
   if (data) {
-    response = await ok(data);
+    response = await HttpResponse.ok(data);
   } else {
-    response = await noContent();
+    response = await HttpResponse.noContent();
   }
+  return response;
+};
+
+
+export const getPlayerByIdService = async (id: number) => {
+  // pedir  pro repisotório de dados
+  const data = await PlayerRepostory.findPlayerById(id);
+  let response = null;
+
+  if(data) {
+    response = HttpResponse.ok(data);
+  } else {
+    response = HttpResponse.noContent();
+  }
+
   return response;
 };
